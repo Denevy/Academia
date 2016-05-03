e91baf138eff704fd0e34adab4f5dc4ce2c4f428
Denevy Saquic <d3n3vy@gmail.com>
2016-05-02 23:40:58 -0600

    Configurando Conexión a PHP 7.XX

--- Academia/php/conexion.php
+++ Academia/php/conexion.php
@@ -1,4 +1,16 @@
 <?php
-	$con = mysql_connect('localhost','root','');
-	mysql_select_db("chronos", $con) or die("No se pudo conectar a la base de datos");
+	/*$con = mysql_connect('localhost','root','D3n3vy&t1z01');
+	mysql_select_db('chronos', $con) or die("No se pudo conectar a la base de datos");*/
+
+	$servername = "localhost";
+	$username = "Academia";
+	$password = "Academia1234/";
+	$dbname = "Academia";
+	// Create connection
+	$con = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
+
 ?>
--- Academia/php/conexion2.php
+++ Academia/php/conexion2.php
@@ -1,8 +1,8 @@
 <?php
 	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "chronos";
+	$username = "Academia";
+	$password = "Academia1234/";
+	$dbname = "Academia";
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
