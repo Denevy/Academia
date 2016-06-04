bc0756cdf4e51aa2bebb13b5f9ec215a4adedbf5
Denevy Saquic <d3n3vy@gmail.com>
2016-06-03 23:28:34 -0600

    subiendo base de datos

--- bd/base.sql
+++ bd/base.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 02-06-2016 a las 00:32:58
+-- Tiempo de generación: 04-06-2016 a las 00:27:49
 -- Versión del servidor: 5.7.12-0ubuntu1
 -- Versión de PHP: 7.0.4-7ubuntu2.1
 
@@ -33,6 +33,15 @@ CREATE TABLE `categoria` (
   `nivelAcademico_idnivelAcademico` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `tipo`, `descripcion`, `nivelAcademico_idnivelAcademico`) VALUES
+(3, 'Ciencias Exactas', 'Ciencias Exactas', 3),
+(4, 'Literatura', 'Literatura', 3),
+(5, 'Programacion', 'Programacion Algoritmos', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +54,17 @@ CREATE TABLE `curso` (
   `descripcion` varchar(1000) NOT NULL,
   `categoria_idcategoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`idcurso`, `nombre`, `descripcion`, `categoria_idcategoria`) VALUES
+(3, 'Matematicas', 'Algebra', 3),
+(4, 'Fisica', 'Leyes de Newton', 3),
+(5, 'Algoritmos', 'Algoritmos computacionales', 5),
+(6, 'Caligrafia', 'Estudio de la Escritura', 4),
+(8, 'Idiomas', 'Estudio de Idiomas', 4);
 
 -- --------------------------------------------------------
 
@@ -62,7 +82,21 @@ CREATE TABLE `estado` (
 --
 
 INSERT INTO `estado` (`idestado`, `tipo`) VALUES
-(1, 'Activo');
+(1, 'Activo'),
+(2, 'Inactivo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Evaluacion`
+--
+
+CREATE TABLE `Evaluacion` (
+  `idEvaluacion` int(11) NOT NULL,
+  `curso` varchar(45) DEFAULT NULL,
+  `estadoEvalucion` int(11) DEFAULT NULL,
+  `idcurso` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -80,7 +114,32 @@ CREATE TABLE `nivelAcademico` (
 --
 
 INSERT INTO `nivelAcademico` (`idnivelAcademico`, `grado`) VALUES
+(3, '1ero. Basico'),
 (1, 'Universitario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opcion`
+--
+
+CREATE TABLE `opcion` (
+  `idopcion` int(11) NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `Preguntas_idPreguntas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Preguntas`
+--
+
+CREATE TABLE `Preguntas` (
+  `idPreguntas` int(11) NOT NULL,
+  `pregunta` varchar(45) DEFAULT NULL,
+  `Evaluacion_idEvaluacion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -98,7 +157,8 @@ CREATE TABLE `rol` (
 --
 
 INSERT INTO `rol` (`idrol`, `privilegio`) VALUES
-(1, 'Administrador');
+(1, 'Administrador'),
+(2, 'Estudiante');
 
 -- --------------------------------------------------------
 
@@ -123,7 +183,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idusuario`, `alias`, `nombres`, `apellidos`, `edad`, `password`, `estado_idestado`, `rol_idrol`, `nivelAcademico_idnivelAcademico`) VALUES
-(1, 'admin', 'Denevy Alexander', 'Saquic Tizol', 25, 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 1, 1);
+(1, 'admin', 'Denevy', 'Saquic', 25, 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 1, 1),
+(2, 'Alex', 'Alexander', 'Saquic', 25, '60c6d277a8bd81de7fdde19201bf9c58a3df08f4', 1, 2, 3);
 
 --
 -- Índices para tablas volcadas
@@ -156,12 +217,32 @@ ALTER TABLE `estado`
   ADD UNIQUE KEY `idestado_UNIQUE` (`idestado`);
 
 --
+-- Indices de la tabla `Evaluacion`
+--
+ALTER TABLE `Evaluacion`
+  ADD PRIMARY KEY (`idEvaluacion`);
+
+--
 -- Indices de la tabla `nivelAcademico`
 --
 ALTER TABLE `nivelAcademico`
   ADD PRIMARY KEY (`idnivelAcademico`),
   ADD UNIQUE KEY `idnivelAcademico_UNIQUE` (`idnivelAcademico`),
   ADD UNIQUE KEY `grado_UNIQUE` (`grado`);
+
+--
+-- Indices de la tabla `opcion`
+--
+ALTER TABLE `opcion`
+  ADD PRIMARY KEY (`idopcion`),
+  ADD KEY `fk_opcion_Preguntas1_idx` (`Preguntas_idPreguntas`);
+
+--
+-- Indices de la tabla `Preguntas`
+--
+ALTER TABLE `Preguntas`
+  ADD PRIMARY KEY (`idPreguntas`),
+  ADD KEY `fk_Preguntas_Evaluacion1_idx` (`Evaluacion_idEvaluacion`);
 
 --
 -- Indices de la tabla `rol`
@@ -190,27 +271,27 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `idcurso` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idcurso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT de la tabla `estado`
 --
 ALTER TABLE `estado`
-  MODIFY `idestado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idestado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `nivelAcademico`
 --
 ALTER TABLE `nivelAcademico`
-  MODIFY `idnivelAcademico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idnivelAcademico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
-  MODIFY `idrol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idrol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
@@ -231,6 +312,18 @@ ALTER TABLE `categoria`
 --
 ALTER TABLE `curso`
   ADD CONSTRAINT `fk_curso_categoria1` FOREIGN KEY (`categoria_idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `opcion`
+--
+ALTER TABLE `opcion`
+  ADD CONSTRAINT `fk_opcion_Preguntas1` FOREIGN KEY (`Preguntas_idPreguntas`) REFERENCES `Preguntas` (`idPreguntas`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `Preguntas`
+--
+ALTER TABLE `Preguntas`
+  ADD CONSTRAINT `fk_Preguntas_Evaluacion1` FOREIGN KEY (`Evaluacion_idEvaluacion`) REFERENCES `Evaluacion` (`idEvaluacion`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `usuario`
